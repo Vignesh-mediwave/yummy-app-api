ac4643a647806a1579adeb6388a281b72c6e0a5c
Vignesh-mediwave <vignesh@mindwaveventures.com>
2022-02-02 15:57:48 +0530

    express and nodemon and server added

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit;
+  }
+  console.log(`Server is running on port ${port}`);
+});
